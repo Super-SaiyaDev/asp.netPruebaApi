e595e7e8fe110f57266c0b5d3e2ca39a5a845884
Edelson Arias <edelsonjaelta@gamil.com>
2024-11-18 10:15:42 -0400

    He configurado una aplicación ASP.NET Core con los siguientes componentes:
    1. **Serilog**: Para el registro de logs.
    2. **Entity Framework Core**: Para la gestión de la base de datos con SQL Server.
    3. **Swagger**: Para la documentación de la API.
    4. **Middleware**: Para manejar excepciones.
    5. **Configuración del puerto**: Cambiado a 5040.
    
    El código incluye la configuración de servicios, middlewares y la ejecución de la aplicación.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/App.jsx'
+import Path from './router/router';
 import './css/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Path />
   </StrictMode>,
 )
---
+++ frontend/src/router/router.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginUser from "../app/login";
+
+
+const Path = () => {
+  return (
+    <Router>
+      <Routes>
+
+        {/* //?  Rutas del login. */}
+        <Route path="/login" element={<LoginUser />} />
+
+        <Route
+          path="*"
+          element={
+            <>
+              <h1
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center",
+                  minHeight: "90vh",
+                }}
+              >
+                not found this path{" "}
+              </h1>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default Path;
